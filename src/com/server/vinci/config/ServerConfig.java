dbe9164855ca18b1d3463e1fee9dc1fd8ad30135
dengdajun <1163388086@qq.com>
2016-11-04 18:03:07 +0800

    config load

--- src/com/server/vinci/config/ServerConfig.java
+++ src/com/server/vinci/config/ServerConfig.java
@@ -1,18 +1,23 @@
 package com.server.vinci.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by Werwolf on 2016/11/4.
  */
 public class ServerConfig {
+    private static final String CONFIG_DIR="server.properties";
     private static final String DEFAULT_CONFIG="server.properties";
     private Map<String,Object>  configs=null;
     public ServerConfig(){
         new ServerConfig(DEFAULT_CONFIG);
     }
     public ServerConfig(String file){
-
+        Properties property=new Properties();
+        property.load(new InputStream(ServerConfig.class.getClassLoader().getResourceAsStream(CONFIG_DIR+file), "utf-8"));
     }
 
 }
